cc83cd84f43301721fa46e15d8638dce4198b0af
Jerome Quelin <jquelin@gmail.com>
2013-02-12 13:14:50 +0100

    initial commit

---
+++ lib/SDLx/GUI.pm
@@ -0,0 +1,39 @@
+use 5.012;
+use strict;
+use warnings;
+
+package SDLx::GUI;
+# ABSTRACT:
+
+1;
+__END__
+
+=head1 SEE ALSO
+
+You can find more information on this module at:
+
+=over 4
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/SDLx-GUI>
+
+=item * See open / report bugs
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=SDLx-GUI>
+
+=item * Git repository
+
+L<http://github.com/jquelin/sdlx-gui>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/SDLx-GUI>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/SDLx-GUI>
+
+=back
+
+
