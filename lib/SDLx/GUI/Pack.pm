0d3e04cfaa694925b078a91aa4d2e4cad5b5d7b9
Jerome Quelin <jquelin@gmail.com>
2013-02-20 16:55:33 +0100

    new SG:Pack object

---
+++ lib/SDLx/GUI/Pack.pm
@@ -0,0 +1,72 @@
+use 5.016;
+use warnings;
+
+package SDLx::GUI::Pack;
+# ABSTRACT: Objects to keep track of pack options
+
+use Moose;
+use MooseX::Has::Sugar;
+use MooseX::SemiAffordanceAccessor;
+
+use SDLx::GUI::Debug qw{ debug };
+use SDLx::GUI::Types;
+
+
+# -- attributes
+
+=attr child
+
+The widget to be packed (a C<SDLx::GUI::Widget> object) - required.
+
+=cut
+
+has child => ( ro, required, isa=>"SDLx::GUI::Widget" );
+
+=attr side
+
+On which side to pack the widget - see C<PackSide> in
+L<SDLx::GUI::Types>. Defaults to C<top>.
+
+=cut
+
+has side  => ( ro, isa=>"PackSide", default=>"top" );
+
+
+=attr parcel
+
+The parcel devoted to the pack (a L<SDLx::Rect> object). See the
+packer algorithm for more information on the parcel.
+
+=attr slave_dims
+
+The dimensions that the child should fill (a L<SDLx::Rect> object).
+See the packer algorithm for more information on the slave dimensions.
+
+=attr clip
+
+A L<SDLx::Rect> used to clip a packed child if there isn't enough place.
+
+=cut
+
+has parcel     => (rw, isa=>"SDLx::Rect" );
+has slave_dims => (rw, isa=>"SDLx::Rect" );
+has clip       => (rw, isa=>"SDLx::Rect", predicate=>"has_clip" );
+
+
+# -- initialization
+
+sub BUILD    { debug( "pack object created: $_[0]\n" ); }
+sub DEMOLISH { debug( "pack object destroyed: $_[0]\n" ); }
+
+
+
+no Moose;
+__PACKAGE__->meta->make_immutable;
+1;
+__END__
+
+=for Pod::Coverage BUILD DEMOLISH
+
+=head1 DESCRIPTION
+
+This class defines objects keeping track of packed widgets.
---
+++ lib/SDLx/GUI/Types.pm
@@ -0,0 +1,27 @@
+use 5.016;
+use warnings;
+
+package SDLx::GUI::Types;
+# ABSTRACT: Types used in the distribution
+
+use Moose::Util::TypeConstraints;
+
+enum 'PackSide' => qw{ top bottom left right };
+
+1;
+__END__
+
+=head1 DESCRIPTION
+
+This module implements the specific types used by the distribution, and
+exports them (exporting is done by L<Moose::Util::TypeConstraints>).
+
+Current types defined:
+
+=over 4
+
+=item * PackSide - a simple enumeration, allowing C<top>, C<bottom>,
+C<left> and C<right>.
+
+=back
+
